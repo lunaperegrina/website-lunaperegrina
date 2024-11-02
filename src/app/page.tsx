c933f0a2902d04c3f79eeb85a9f251e2d5ef8f5b
Luna Peregrina <57403682+lunaperegrina@users.noreply.github.com>
2024-11-02 00:17:58 -0300

    feat(home): create initial homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,127 @@
 import Image from "next/image";
+import { House, File, Info, Contact, Heart } from "lucide-react"
+import { Icons } from "@/components/icons";
+
+function BaseButton({ children }: { children: React.ReactNode }) {
+  return (
+    <button className="p-3 gap-2 rounded-full border flex flex-row items-center hover:text-purple-500 transition duration-300">
+      {children}
+    </button>
+  );
+}
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
+    <div>
+      {/* <nav className="flex flex-row justify-between m-4 max-w-7xl mx-auto border-b pb-4 px-4">
+        <BaseButton>
+          <Heart className="w-4 h-4" /> lunaperegrina
+        </BaseButton>
+        <ul className="flex gap-8 p-3 rounded-full border">
+          <li>
+            <a href="" className="flex gap-2 items-center hover:text-purple-500 transition duration-300">
+              <House className="w-4 h-4" />
+              Home
+            </a>
           </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          <li>
+            <a href="" className="flex gap-2 items-center hover:text-purple-500 transition duration-300">
+              <File className="w-4 h-4" />
+              My Projects
+            </a>
+          </li>
+          <li>
+            <a href="" className="flex gap-2 items-center hover:text-purple-500 transition duration-300">
+              <Info className="w-4 h-4" />
+              About
+            </a>
+          </li>
+        </ul>
+        <BaseButton>
+          <Contact className="w-4 h-4" />
+          Contact
+        </BaseButton>
+      </nav> */}
+      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div> <div className="flex flex-col justify-center text-center h-[90vh]">
+        <div className="flex flex-row mx-auto gap-4 mb-4">
+          <Image
+            src="/images/icon-luna-apple.png"
+            alt="Luna Peregrina"
+            width={100}
+            height={100}
+            className="border w-16 h-16 object-cover rounded-full"
+          />
+          <div className="w-80 h-16 text-xl content-center gap-2 text-center rounded-full border">
+            Hello, I'm Luna Peregrina! 🌙
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="mx-auto">
+          <div className="flex flex-row justify-center">
+            <div className="text-7xl font-semibold text-purple-500">CLIENT</div>
+            <div className="text-start flex flex-row items-center ml-4">
+              // The best experience
+              <br />
+              always first
+            </div>
+          </div>
+          <div className="flex flex-row justify-center">
+            <div className="text-start flex flex-row items-center mr-4">
+              // 📍 Porto Seguro,
+              <br />
+              Bahia, Brasil 🥥🇧🇷
+            </div>
+            <div className="text-7xl font-semibold text-yellow-300">FOCUSED</div>
+          </div>
+          {/* <div className="text-7xl font-semibold text-yellow-300">FOCUSED</div> */}
+          <div className="flex flex-row justify-center">
+            <div className="text-start flex flex-row items-center mr-4">
+              <div className="text-7xl font-semibold">FULLSTACK</div>
+              <button className="p-6 gap-2 rounded-full border flex flex-row items-center h-16 text-lg">
+                <div className="relative mb-0.5 z-10">
+                  {/* <div className="absolute w-2 h-2 rounded-full mt-0.5 bg-stone-400 sm:w-3 sm:h-3 md:h-4 md:w-4 flex items-center justify-center" /> */}
+                  <div className="w-2 h-2 rounded-full mt-0.5 bg-yellow-300 sm:w-3 sm:h-3 md:h-4 md:w-4 animate-pulse pulse-slow" />
+                </div>
+                Comming Soon...
+              </button>
+            </div>
+          </div>
+          <div className="flex flex-row justify-center">
+            <div className="text-start flex flex-row items-center mr-4">
+              // Next, Nest, Vue,
+              <br />
+              Python and more!
+            </div>
+            <div className="text-7xl font-semibold "><span className="text-cyan-300">DEVELOPER.</span></div>
+          </div>
+        </div>
+        <h2 className="text-3xl font-semibold mt-8 mx-auto text-center w-[800px]">
+          Creating software that enhances your experience with <span className="text-cyan-300">productivity</span>, <span className="text-yellow-300">appeal</span>, and <span className="text-purple-500">asthetic</span>.
+        </h2>
+      </div>
+      <div className="flex max-w-7xl mx-auto justify-center px-8 py-4">
+        <div className="flex flex-row gap-2">
+          <div className="bg-black w-10 h-10 rounded-full flex justify-center items-center">
+            <a target="_blank" href="https://github.com/lunaperegrina">
+              <Icons.gitHub className="w-7 h-7 text-white" />
+            </a>
+          </div>
+          <div className="bg-black w-10 h-10 rounded-full flex justify-center items-center">
+            <a target="_blank" href="https://www.instagram.com/eitacomoperegrina">
+              <Icons.instagram className="w-7 h-7 text-white" />
+            </a>
+          </div>
+          <div className="bg-black w-10 h-10 rounded-full flex justify-center items-center">
+            <a target="_blank" href="https://www.linkedin.com/in/lunaperegrina">
+              <Icons.linkedin className="w-6 h-6 text-white" />
+            </a>
+          </div>
+          <div className="bg-black w-10 h-10 rounded-full flex justify-center items-center">
+            <a target="_blank" href="https://bsky.app/profile/lunaperegrina.dev">
+              <Icons.bluesky className="w-6 h-6 text-white" />
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
