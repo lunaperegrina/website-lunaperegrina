c67a07e2c33143dd42828834222c17b906337336
Luna Peregrina <lunaperegrinamoon@gmail.com>
2024-11-30 17:07:47 -0300

    style(home): apply responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,14 @@ function BaseButton({ children }: { children: React.ReactNode }) {
   );
 }
 
+function TitleColored({ children, className }:
+  { children: React.ReactNode, className?: string }
+) {
+  return (
+    <h1 className={`font-bold text-4xl sm:text-7xl ${className}`}>{children}</h1>
+  );
+}
+
 export default function Home() {
   return (
     <div>
@@ -49,59 +57,59 @@ export default function Home() {
             alt="Luna Peregrina"
             width={100}
             height={100}
-            className="border w-16 h-16 object-cover rounded-full"
+            className="border w-12 sm:w-16 h-12 sm:h-16 object-cover rounded-full"
           />
-          <div className="w-80 h-16 text-xl content-center gap-2 text-center rounded-full border">
+          <div className="w-60 sm:w-80 sm:80 h-12 sm:h-16 text-sm sm:text-xl content-center gap-2 text-center rounded-full border">
             Hello, I'm Luna Peregrina! 🌙
           </div>
         </div>
         <div className="mx-auto">
           <div className="flex flex-row justify-center">
-            <div className="text-7xl font-semibold text-purple-500">CLIENT</div>
-            <div className="text-start flex flex-row items-center ml-4">
+            <TitleColored className="text-purple-500">CLIENT</TitleColored>
+            <div className="text-start flex flex-row items-center ml-4 text-xs sm:text-base">
               // The best experience
               <br />
               always first
             </div>
           </div>
           <div className="flex flex-row justify-center">
-            <div className="text-start flex flex-row items-center mr-4">
+            <div className="text-start flex flex-row items-center mr-4 text-xs sm:text-base">
               // 📍 Porto Seguro,
               <br />
               Bahia, Brasil 🥥🇧🇷
             </div>
-            <div className="text-7xl font-semibold text-yellow-300">FOCUSED</div>
+            <TitleColored className="text-yellow-300">FOCUSED</TitleColored>
           </div>
           {/* <div className="text-7xl font-semibold text-yellow-300">FOCUSED</div> */}
           <div className="flex flex-row justify-center">
             <div className="text-start flex flex-row items-center mr-4">
-              <div className="text-7xl font-semibold">FULLSTACK</div>
-              <button className="p-6 gap-2 rounded-full border flex flex-row items-center h-16 text-lg">
+              <TitleColored>FULLSTACK</TitleColored>
+              <button className="p-6 gap-2 rounded-full border flex flex-row items-center h-12 sm:h-16 text-base sm:text-lg">
                 <div className="relative mb-0.5 z-10">
                   {/* <div className="absolute w-2 h-2 rounded-full mt-0.5 bg-stone-400 sm:w-3 sm:h-3 md:h-4 md:w-4 flex items-center justify-center" /> */}
                   <div className="w-2 h-2 rounded-full mt-0.5 bg-yellow-300 sm:w-3 sm:h-3 md:h-4 md:w-4 animate-pulse pulse-slow" />
                 </div>
-                Comming Soon...
+                Soon...
               </button>
             </div>
           </div>
           <div className="flex flex-row justify-center">
-            <div className="text-start flex flex-row items-center mr-4">
+            <div className="text-start flex flex-row items-center mr-4 text-xs sm:text-base">
               // Next, Nest, Vue,
               <br />
               Python and more!
             </div>
-            <div className="text-7xl font-semibold "><span className="text-cyan-300">DEVELOPER.</span></div>
+              <TitleColored className="text-cyan-300">DEVELOPER</TitleColored>
           </div>
         </div>
-        <h2 className="text-3xl font-semibold mt-8 mx-auto text-center w-[800px]">
+        <h2 className="text-base sm:text-3xl font-semibold mt-8 mx-auto text-center w-3/4">
           Creating software that enhances your experience with <span className="text-cyan-300">productivity</span>, <span className="text-yellow-300">appeal</span>, and <span className="text-purple-500">asthetic</span>.
         </h2>
       </div>
       <div className="flex max-w-7xl mx-auto justify-center px-8 py-4">
         <div className="flex flex-row gap-2">
           <div className="bg-black w-10 h-10 rounded-full flex justify-center items-center">
-            <a target="_blank" href="https://github.com/lunaperegrina">
+            <a rel="noopener noreferrer" target="_blank" href="https://github.com/lunaperegrina">
               <Icons.gitHub className="w-7 h-7 text-white" />
             </a>
           </div>
@@ -125,3 +133,4 @@ export default function Home() {
     </div>
   );
 }
+
